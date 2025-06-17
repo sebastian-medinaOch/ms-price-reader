700d38dbbc5e89164bcde5bd4a28cea539761e33
sebastian-medina-pragma <sebastian.medina@pragma.com.co>
2025-06-17 07:13:18 -0500

    Se cambia el nombre del main class

--- src/main/java/com/smo/price/PriceReaderApplication.java
+++ src/main/java/com/smo/price/PriceReaderApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ManagementPriceApplication {
+public class PriceReaderApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ManagementPriceApplication.class, args);
+        SpringApplication.run(PriceReaderApplication.class, args);
     }
 }
