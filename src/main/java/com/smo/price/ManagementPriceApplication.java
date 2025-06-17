26ef1187d0b954057a3c3d97e556c14efae7d558
sebastian-medina-pragma <sebastian.medina@pragma.com.co>
2025-06-17 07:03:51 -0500

    Se corrigen y agregan archivos de configuración

--- src/main/java/com/smo/price/ManagementPriceApplication.java
+++ src/main/java/com/smo/price/ManagementPriceApplication.java
@@ -2,14 +2,10 @@ package com.smo.price;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
-public class OrchestratorApplication {
-
+public class ManagementPriceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(OrchestratorApplication.class, args);
+        SpringApplication.run(ManagementPriceApplication.class, args);
     }
-
 }
